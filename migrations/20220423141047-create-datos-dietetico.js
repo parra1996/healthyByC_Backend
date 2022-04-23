e80b3cdd3f7e5150a014e9e9a8607aba584507d2
“juanP1996” <parrajp1996@gmail.com>
2022-04-23 16:59:41 +0200

    feat: modelo de datos_dietetico con su relacion hecha

--- migrations/20220423141047-create-datos-dietetico.js
+++ migrations/20220423141047-create-datos-dietetico.js
@@ -3,16 +3,49 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Datos_dieteticos', {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       numero_comidas_diarias: {
         type: Sequelize.STRING
       },
+      nivel_apetito_diario: {
+        type: Sequelize.STRING
+      },
+      horas_hambre_mas_intensidad: {
+        type: Sequelize.STRING
+      },
+      que_suele_comer_diario: {
+        type: Sequelize.STRING
+      },
+      que_suele_comer_fds: {
+        type: Sequelize.STRING
+      },
+      tipo_preparaciones_aplicadas_comidas: {
+        type: Sequelize.STRING
+      },
+      comidas_a_evitar: {
+        type: Sequelize.STRING
+      },
+      alimentos_preferibles_a_su_plan: {
+        type: Sequelize.STRING
+      },
+      lugares_habituales_de_compra: {
+        type: Sequelize.STRING
+      },
+      alergias: {
+        type: Sequelize.BOOLEAN
+      },
       createdAt: {
-        allowNull: false,
+        allowNull: false, 
         type: Sequelize.DATE
       },
       updatedAt: {
--- models/datos_dietetico.js
+++ models/datos_dietetico.js
@@ -10,11 +10,23 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.User, {
+        foreignKey: 'datos_dieteticosID'
+      });
     }
   }
   Datos_dietetico.init({
-    numero_comidas_diarias: DataTypes.STRING
+    numero_comidas_diarias: DataTypes.STRING,
+    nivel_apetito_diario: DataTypes.STRING,
+    horas_hambre_mas_intensidad: DataTypes.STRING,
+    que_suele_comer_diario: DataTypes.STRING,
+    que_suele_comer_fds: DataTypes.STRING,
+    tipo_preparaciones_aplicadas_comidas: DataTypes.STRING,
+    comidas_a_evitar: DataTypes.STRING,
+    alimentos_preferibles_a_su_plan: DataTypes.STRING,
+    lugares_habituales_de_compra: DataTypes.STRING,
+    alergias: DataTypes.BOOLEAN,
+
   }, {
     sequelize,
     modelName: 'Datos_dietetico',
