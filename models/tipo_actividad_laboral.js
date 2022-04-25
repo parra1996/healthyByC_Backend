628518cb3824b25b969ef40a09e94a84395c0ed7
“juanP1996” <parrajp1996@gmail.com>
2022-04-25 17:10:31 +0200

    feat: creado modelo de tipo de actividad laboral sin sus relaciones

---
+++ models/tipo_actividad_laboral.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tipo_actividad_laboral extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tipo_actividad_laboral.init({
+    frecuencia: DataTypes.STRING,
+    duracion: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Tipo_actividad_laboral',
+  });
+  return Tipo_actividad_laboral;
+};
